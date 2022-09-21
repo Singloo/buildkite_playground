1550b2fa1438ed3817da24adb892078780200204
Rick nagual <singloo.yang@gmail.com>
2022-09-21 09:36:21 +0800

    docker

--- docker-build-prod.sh
+++ docker-build-prod.sh
@@ -1,2 +1,1 @@
 docker build -f ./docker/Dockerfile -t playground/buildkite .
-docker-compose -f ./docker/docker-compose.prod.yml up
---
+++ scripts/logDir.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const BUILD_DIR_PATH = path.resolve(__dirname, "..", "build");
+
+const loopDir = (pth) => {
+  const fileStat = fs.statSync(pth);
+  if (fileStat.isDirectory) {
+    const filenames = fs.readdirSync(pth);
+    return filenames.map((filename) => loopDir(path.resolve(pth, filename)));
+  }
+  console.log("FILE", pth);
+  return pth;
+};
+
+const allPaths = loopDir(BUILD_DIR_PATH).flat();
+console.log("FOUND FILE", allPaths.length);
