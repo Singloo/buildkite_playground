19eef9a30f0e6bfbf9970aecc0da3b0de0f1dc8c
Rick nagual <singloo.yang@gmail.com>
2022-10-17 08:58:25 +0800

    fix

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,10 +1,4 @@
 # usr/bin/bash
 set -x
 docker-compose -f ./docker/docker-compose.prod.yml up
-# node ./scripts/logDir.js
-ls
-cd ./public
-ls
-cd ../src
-ls
 docker-compose -f ./docker/docker-compose.prod.yml down
