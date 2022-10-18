5eee9543039fda95c9ce3f594f88002a87ef3c83
Rick nagual <singloo.yang@gmail.com>
2022-10-18 09:28:50 +0800

    try various

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,6 +1,8 @@
 # usr/bin/bash
 set -x
-chmod a+rw .
+chmod a+rw ./src/*
+chmod a+rw ./src/
+chmod a+rw ./src
 docker-compose -f ./docker-compose.prod.yml up
 ls
 docker-compose -f ./docker-compose.prod.yml down
