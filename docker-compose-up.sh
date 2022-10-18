d64601a8357995fa45b33ee4dbb6542d5afc677f
Rick nagual <singloo.yang@gmail.com>
2022-10-18 10:13:14 +0800

    fix

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,6 +1,5 @@
 # usr/bin/bash
-set -x
 pwd
-docker compose -f ./docker-compose.prod.yml up
+docker-compose -f ./docker-compose.prod.yml up
 ls
-docker compose -f ./docker-compose.prod.yml down
+docker-compose -f ./docker-compose.prod.yml down
