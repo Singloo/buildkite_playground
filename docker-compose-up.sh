41bc4b4a837a92af4d6bb5fc79996f6b1b4a7aca
Rick nagual <singloo.yang@gmail.com>
2022-10-18 10:06:35 +0800

    volume in docker compose cmd

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,6 +1,6 @@
 # usr/bin/bash
 set -x
 pwd
-docker-compose -f ./docker-compose.prod.yml up
+docker-compose -v ./public:./public -f ./docker-compose.prod.yml up
 ls
 docker-compose -f ./docker-compose.prod.yml down
