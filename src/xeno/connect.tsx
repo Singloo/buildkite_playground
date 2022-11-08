596b5f30ca0eeaa7f76405ed8de2ea67bdd7752a
Rick nagual <singloo.yang@gmail.com>
2022-11-08 08:38:28 +0800

    fix

--- src/xeno/connect.tsx
+++ src/xeno/connect.tsx
@@ -1,14 +1,18 @@
-import React, {useEffect, useRef} from 'react';
-import {Xeno} from './xeno';
-import {IReactComponent, HandlerFunction} from './type';
+import { useEffect, useRef } from "react";
+import { Xeno } from "./xeno";
+import {
+  IReactComponent,
+  TXenoMessage,
+  FuncWithEventNameHandler,
+} from "./type";
 export const connectXeno =
-  <T,>(xeno: Xeno<T>) =>
+  <T extends TXenoMessage>(xeno: Xeno<T>) =>
   (Comp: IReactComponent<any>) => {
     const EnhancedComp = (props: any) => {
       const unlistens = useRef<Function[]>([]);
-      const on = <K extends keyof T>(
-        eventName: K,
-        handler: HandlerFunction<T, K>,
+      const on: FuncWithEventNameHandler<T, () => void> = (
+        eventName,
+        handler
       ) => {
         const unlisten = xeno.on(eventName, handler);
         unlistens.current.push(unlisten);
@@ -16,15 +20,13 @@ export const connectXeno =
       };
       useEffect(() => {
         const cleanListeners = () => {
-          unlistens.current.forEach(func => func());
+          unlistens.current.forEach((func) => func());
         };
-        return () => {
-          cleanListeners();
-        };
+        return cleanListeners;
       }, []);
 
       return <Comp {...props} on={on} trigger={xeno.trigger} />;
     };
-    EnhancedComp.displayName = 'Xeno-connected-' + Comp.displayName;
+    EnhancedComp.displayName = "Xeno-connected-" + Comp.displayName;
     return EnhancedComp;
   };
