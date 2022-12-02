4daa4ec25baae11265710f60d05cb11e040ae351
Rick nagual <singloo.yang@gmail.com>
2022-12-02 10:55:22 +0800

    feat: union resolve

--- src/withXeno/inversify.ts
+++ src/withXeno/inversify.ts
@@ -1,6 +1,7 @@
-import { Container, injectable } from 'inversify';
+import { Container, injectable } from "inversify";
 
-const CLASS_SYMBOL = Symbol('classSymbol');
+const CLASS_SYMBOL = Symbol("classSymbol");
+const SINGLETON_SYMBOL = Symbol("isSingleton");
 export const container = new Container();
 
 const USE_SYMBOL_AS_IDENTIFIER = false;
@@ -13,16 +14,19 @@ type InjectableConfig = {
 export const Injectable = (
   { name, singleton }: InjectableConfig = {
     name: undefined,
-    singleton: undefined,
-  },
+    singleton: false,
+  }
 ) => {
   return (target: ClassLike<any>) => {
     const res = injectable()(target);
-    let classIdentifier = USE_SYMBOL_AS_IDENTIFIER ? Symbol.for(target.name) : target;
-    if (typeof name !== 'undefined' && USE_SYMBOL_AS_IDENTIFIER) {
+    let classIdentifier = USE_SYMBOL_AS_IDENTIFIER
+      ? Symbol.for(target.name)
+      : target;
+    if (typeof name !== "undefined" && USE_SYMBOL_AS_IDENTIFIER) {
       classIdentifier = Symbol(name);
       Reflect.defineMetadata(CLASS_SYMBOL, classIdentifier, target);
     }
+    Reflect.defineMetadata(SINGLETON_SYMBOL, !!singleton, target);
     const bind = container.bind<typeof target>(classIdentifier).to(target);
     if (singleton) bind.inSingletonScope();
 
@@ -38,29 +42,73 @@ const getClassSymbol = (target: ClassLike<any>) => {
 
 export const checkIfContainsCircleDeps = (
   chain: ClassLike<any>[],
-  nowResolving: ClassLike<any>,
+  nowResolving: ClassLike<any>
 ) => {
   const hasBeenRequired = chain.some((c) => c === nowResolving);
   return hasBeenRequired;
 };
 
 const isClass = (target: new (...args: any[]) => any) => {
-  return typeof target === 'function' && /^class\s/.test(target.prototype.constructor.toString());
+  return (
+    typeof target === "function" &&
+    /^class\s/.test(target.prototype.constructor.toString())
+  );
 };
-export const resolve = <T extends any>(target: ClassLike<T>): T => {
-  if (!isClass(target)) throw new Error('Invalid target, expect Class');
-  const identifier = getClassSymbol(target);
-  const paramTypes = Reflect.getMetadata('design:paramtypes', target);
+export const resolve = <T extends any>(
+  target: ClassLike<T>,
+  sharedContaienr?: Container
+): T => {
+  if (!isClass(target)) throw new Error("Invalid target, expect Class");
+  const identifier = getClassSymbol(target); // identifier of class
+  const paramTypes = Reflect.getMetadata("design:paramtypes", target);
   if (!paramTypes?.length) {
+    // no parameters, create an instance
     const isBound = container.isBound(identifier);
-    if (isBound) return container.get(identifier);
+    if (isBound) {
+      const isSingleton = Reflect.getMetadata(SINGLETON_SYMBOL, target);
+      const instance = container.get(identifier) as T;
+      // if target is a singleton or no shared container exists, return instance
+      if (isSingleton || !sharedContaienr) return instance;
+      // for non-singleton class, register instance, return instance from shared container
+      if (!sharedContaienr.isBound(identifier)) {
+        sharedContaienr.bind(identifier).toConstantValue(instance);
+      }
+      return sharedContaienr.get(identifier);
+    }
     return new target();
   }
 
-  const args: any[] = (paramTypes as any[]).map(resolve);
+  const args: any[] = (paramTypes as any[]).map((param) =>
+    resolve(param, sharedContaienr)
+  );
   return new target(...args);
 };
 
+function unionResolve<T1>(...args: [ClassLike<T1>]): [T1];
+function unionResolve<T1, T2>(
+  ...args: [ClassLike<T1>, ClassLike<T2>]
+): [T1, T2];
+function unionResolve<T1, T2, T3>(
+  ...args: [ClassLike<T1>, ClassLike<T2>, ClassLike<T3>]
+): [T1, T2, T3];
+function unionResolve<T1, T2, T3, T4>(
+  ...args: [ClassLike<T1>, ClassLike<T2>, ClassLike<T3>, ClassLike<T4>]
+): [T1, T2, T3, T4];
+function unionResolve<T1, T2, T3, T4, T5>(
+  ...args: [
+    ClassLike<T1>,
+    ClassLike<T2>,
+    ClassLike<T3>,
+    ClassLike<T4>,
+    ClassLike<T5>
+  ]
+): [T1, T2, T3, T4, T5];
+function unionResolve(...targets: ClassLike<any>[]): any[] {
+  const sharedContainer = new Container();
+  return targets.map((target) => resolve(target, sharedContainer));
+}
+
+export { unionResolve };
 export const Instantiable = () => {
   return (target: ClassLike<any>) => {};
 };
