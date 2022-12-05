2a6c3303b9c11759783d3bf79c9cadcfbef18afe
Rick nagual <singloo.yang@gmail.com>
2022-12-05 09:09:03 +0800

    chores

--- src/withXeno/components/Demo1/components/Box/index.ts
+++ src/withXeno/components/Demo1/components/Box/index.ts
@@ -3,7 +3,9 @@ import { MBox } from "./Box";
 import { connectStores } from "src/withXeno/util/connect";
 import { Demo1Store } from "src/withXeno/stores/Demo1/demo1.store";
 import { BoxManagerStore } from "src/withXeno/stores/BoxManager/boxManager.store";
+import { getDynamicComp } from "src/withXeno/components/DynamicComp";
 
+// stores have been determined at this time. 
 const Enhanced = connectStores({ demo1Store: Demo1Store }, BoxController)(MBox);
 
 const getBox = () =>
@@ -13,4 +15,6 @@ const getBox = () =>
   )(MBox);
 export default Enhanced;
 
-export { getBox };
+// stores have same life cycle as react component
+const DynamicBox = getDynamicComp(getBox);
+export { DynamicBox };
